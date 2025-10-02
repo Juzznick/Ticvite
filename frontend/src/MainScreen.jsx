f3bd8e7d8353ffe277b43be613b4a9ce1a8bfca1
Juzznick <xnick126@gmail.com>
2025-10-02 04:21:58 +0400

    added clock

--- frontend/src/MainScreen.jsx
+++ frontend/src/MainScreen.jsx
@@ -3,13 +3,13 @@ import './MainScreen.css'
 function MainScreen () {
 
 
-    const [time, setTime] = useState("hey");
+    const [time, setTime] = useState(new Date().toLocaleTimeString());
     useEffect(() => {
-        const timer = setTimeout(() => {
-            setTime("8:30");
-        }, 2000);
+        const timer = setInterval(() => {
+            setTime(new Date().toLocaleTimeString());
+        }, 1000);
 
-        return () => clearTimeout(timer);
+        return () => clearInterval(timer);
     }, []);
 
 
@@ -20,7 +20,7 @@ function MainScreen () {
                 <div className='bg-[#ACB695] h-[25vw] w-[25vw] rounded-4xl'>-</div>
                 <div className='grid grid-cols-1 grid-rows-2 gap-10'>
                     <div id="time" className='bg-[#D4AD81] w-[25vw] rounded-4xl flex items-center justify-center'>
-                        <h1 className='text-9xl text-[#404040]'>{time}</h1></div>
+                        <h1 className='text-4xl text-[#404040]'>{time}</h1></div>
                     <div className='bg-[#E75C43]  w-[25vw] rounded-4xl'>-</div>
                 </div>
                 <div className='bg-[#E79B43] h-[18vw] col-span-3 rounded-4xl '>-</div>
