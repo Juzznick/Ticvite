ed8f3dd3d457cfbed6181bc02b34bbb2b0727984
Juzznick <xnick126@gmail.com>
2025-10-03 23:58:35 +0400

    updated login side image

--- frontend/src/LoginScreen.jsx
+++ frontend/src/LoginScreen.jsx
@@ -2,10 +2,9 @@ const LoginScreen = () => {
     return (
         <div className="flex justify-center items-center w-screen">
             <div className="bg-[#ACB695] h-[85vh] w-[80vw] rounded-2xl">
-                <div>
-                    <img src="/images/loginimg.png" />
+                <div className="flex justify-center items-center">
+                    <img src="/images/login-sideimg.png" />
                 </div>
-                <h1>Login</h1>
             </div>
         </div>
 
