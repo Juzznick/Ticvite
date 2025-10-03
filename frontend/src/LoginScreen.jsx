7aacb6c68f3252391a9cd9ef637b0d2c0adab53d
Juzznick <xnick126@gmail.com>
2025-10-03 23:43:37 +0400

    Added login page

---
+++ frontend/src/LoginScreen.jsx
@@ -0,0 +1,16 @@
+const LoginScreen = () => {
+    return (
+        <div className="flex justify-center items-center w-screen">
+            <div className="bg-[#ACB695] h-[85vh] w-[80vw] rounded-2xl">
+                <div>
+                    <img src="/images/loginimg.png" />
+                </div>
+                <h1>Login</h1>
+            </div>
+        </div>
+
+
+    );
+}
+
+export default LoginScreen;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,11 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import "tailwindcss";
 import MainScreen from './MainScreen'
+import LoginScreen from './LoginScreen';
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <MainScreen/>
+    <LoginScreen/>
   </StrictMode>,
 )
