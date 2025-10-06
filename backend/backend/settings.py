8444d0aee762fbd099cc8940dfd55ee04d75fbb8
Juzznick <xnick126@gmail.com>
2025-10-06 04:09:43 +0400

    notes model

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -43,7 +43,11 @@ INSTALLED_APPS = [
     'base' #
 ]
 
-
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
