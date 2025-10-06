8a5b59fbc32753dd1c198f75a79b40da51c833eb
Juzznick <xnick126@gmail.com>
2025-10-06 05:03:07 +0400

    created api

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,10 +1,13 @@
-from django.urls import path
+
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
+from .views import get_notes
+from django.urls import path
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('notes/', get_notes)
 ]
