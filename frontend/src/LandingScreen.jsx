77e5bb6c5f81313e47e55ed9e93d95a7faa8369d
Juzznick <xnick126@gmail.com>
2025-10-07 23:48:13 +0400

    added nav

--- frontend/src/LandingScreen.jsx
+++ frontend/src/LandingScreen.jsx
@@ -1,6 +1,18 @@
 const LandingScreen = () => {
     return (
-        <h1 className="font-flubber text-coalblack text-6xl">Focus. Finish. Repeat.</h1>
+        <div className="h-screen w-screen flex justify-center items-center">
+            <nav className="fixed top-0 flex justify-center mt-5">
+                <ul className="flex w-[75vw] justify-around">
+                    <li className="text-2xl text-coalblack">Home</li>
+                    <li className="text-2xl text-coalblack">Features</li>
+                    <li className="text-2xl text-coalblack">Contribute</li>
+                    <li className="text-2xl text-coalblack">Login</li>
+
+                </ul>
+            </nav>
+            <h1 className="font-flubber text-coalblack text-6xl">Focus. Finish. Repeat.</h1>
+        </div>
+
     );
 }
 
