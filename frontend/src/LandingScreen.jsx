6e18d3dbe0ca3ab9f2f79c65f2364df5cd290bcb
Juzznick <xnick126@gmail.com>
2025-10-07 23:13:37 +0400

    added landing page

---
+++ frontend/src/LandingScreen.jsx
@@ -0,0 +1,7 @@
+const LandingScreen = () => {
+    return (
+        <h1 className="font-flubber text-coalblack text-6xl">Focus. Finish. Repeat.</h1>
+    );
+}
+
+export default LandingScreen;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,9 +4,11 @@ import "tailwindcss";
 import { BrowserRouter, Routes, Route } from 'react-router';
 import ReactDOM from "react-dom/client";
 
+import LandingScreen from './LandingScreen';
 import MainScreen from './MainScreen'
 import LoginScreen from './LoginScreen';
 import SignupScreen from './SignupScreen';
+
 
 
 
@@ -15,7 +17,8 @@ const root = document.getElementById("root");
 ReactDOM.createRoot(root).render(
   <BrowserRouter>
     <Routes>
-      <Route path='/' element={<MainScreen/>}></Route>
+      <Route path='/' element={<LandingScreen/>} ></Route>
+      <Route path='dash' element={<MainScreen/>}></Route>
       <Route path='login' element={<LoginScreen/>}></Route>
       <Route path='signup' element={<SignupScreen/>} ></Route>
     </Routes>
