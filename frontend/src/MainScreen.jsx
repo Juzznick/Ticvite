8b5b2a1ae66c0ee17781e739782db9d7bce06579
Juzznick <xnick126@gmail.com>
2025-10-09 23:59:18 +0400

    dash

--- frontend/src/MainScreen.jsx
+++ frontend/src/MainScreen.jsx
@@ -6,7 +6,7 @@ function MainScreen() {
     const [time, setTime] = useState(new Date().toTimeString().slice(0, 5));
     useEffect(() => {
         const timer = setInterval(() => {
-            setTime(new Date().toTimeString().slice(0, 5));
+            setTime(new Date().toTimeString());
         }, 1000);
 
         return () => clearInterval(timer);
@@ -17,12 +17,12 @@ function MainScreen() {
 
         <div className="flex flex-col items-center justify-evenly  h-screen w-[90vw]" >
             <div className='flex justify-between w-[100%]'>
-                <div className='bg-[#ECE8C5] h-[25vw] w-[25vw] rounded-4xl'></div>
-                <div className='bg-[#ACB695] h-[25vw] w-[25vw] rounded-4xl'></div>
+                <div className='bg-[#ECE8C5] h-[25vw] w-[28vw] rounded-4xl'></div>
+                <div className='bg-[#ACB695] h-[25vw] w-[28vw] rounded-4xl'></div>
                 <div className='grid grid-cols-1 grid-rows-2 gap-10'>
-                    <div id="time" className='bg-[#D4AD81] w-[25vw] rounded-4xl flex items-center justify-center'>
+                    <div id="time" className='bg-[#D4AD81] w-[28vw] rounded-4xl flex items-center justify-center'>
                         <h1 className='text-9xl text-[#404040] font-flubber pt-7'>{time}</h1></div>
-                    <div className='bg-[#E75C43]  w-[25vw] rounded-4xl'></div>
+                    <div className='bg-[#E75C43]  w-[28vw] rounded-4xl'></div>
                 </div>
             </div>
             <div className='bg-[#E79B43] h-[35vh] w-[100%] rounded-4xl '>-</div>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,7 +3,7 @@ const Navbar = () => {
         <div className="h-screen w-screen flex justify-center items-center">
                 <nav className="fixed top-0 flex justify-center mt-5">
                     <ul className="flex w-[75vw] justify-around">
-                        <li className="text-2xl text-coalblack cursor-pointer">Home</li>
+                        <li className="text-white cursor-pointer  bg-[#E79B43] pl-6 pr-6 flex items-center rounded-2xl">Home</li>
                         <li className="text-2xl text-coalblack cursor-pointer">Features</li>
                         <li className="text-2xl text-coalblack cursor-pointer">Contribute</li>
                         <a href="/login" className="text-white cursor-pointer bg-[#E75C43] pl-6 pr-6 flex items-center rounded-2xl">
