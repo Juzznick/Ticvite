f0a31b1bd71ee2c2f8d77ac7372250224b20d6d1
Juzznick <xnick126@gmail.com>
2025-10-09 23:45:35 +0400

    landing screen

---
+++ frontend/src/components/Features.jsx
@@ -0,0 +1,9 @@
+const Features = () => {
+    return (
+        <div className="h-screen w-screen flex items-center justify-center">
+            <h1 className="font-flubber text-coalblack text-6xl">Features.</h1>
+        </div>
+    );
+}
+
+export default Features;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+const Navbar = () => {
+    return(
+        <div className="h-screen w-screen flex justify-center items-center">
+                <nav className="fixed top-0 flex justify-center mt-5">
+                    <ul className="flex w-[75vw] justify-around">
+                        <li className="text-2xl text-coalblack cursor-pointer">Home</li>
+                        <li className="text-2xl text-coalblack cursor-pointer">Features</li>
+                        <li className="text-2xl text-coalblack cursor-pointer">Contribute</li>
+                        <a href="/login" className="text-white cursor-pointer bg-[#E75C43] pl-6 pr-6 flex items-center rounded-2xl">
+                            <li>Login</li>
+                        </a>
+                        
+
+                    </ul>
+                </nav>
+                <h1 className="font-flubber text-coalblack text-6xl">Focus. Finish. Repeat.</h1>
+            </div>
+    );
+}
+
+export default Navbar;
