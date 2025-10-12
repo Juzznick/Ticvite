1cf52d05de218bb02162636744a80b57c63c051e
Juzznick <xnick126@gmail.com>
2025-10-12 23:57:46 +0400

    theme changes

--- frontend/src/DashScreen.jsx
+++ frontend/src/DashScreen.jsx
@@ -18,13 +18,15 @@ function DashScreen() {
         <div className="flex flex-col items-center justify-evenly  h-screen w-[90vw]" >
             <div className='flex justify-between w-[100%]'>
                 <div className='bg-[#ECE8C5] h-[25vw] w-[28vw] rounded-4xl'></div>
-                <div className='bg-[#ACB695] h-[25vw] w-[28vw] rounded-4xl'></div>
+                <div className='bg-[#ACB695] h-[25vw] w-[28vw] rounded-4xl flex justify-center items-center'>
+                    <h1 className=' text-2xl font-flubber text-textgrey'>Random Daily Quote Placeholder</h1>
+                </div>
                 <div className='grid grid-cols-1 grid-rows-2 gap-10'>
                     <div id="time" className='bg-[#D4AD81] w-[28vw] rounded-4xl flex items-center justify-center'>
-                        <h1 className='text-9xl text-[#404040] font-flubber pt-7'>{time}</h1>
+                        <h1 className='text-9xl text-textgrey font-flubber pt-7'>{time}</h1>
                     </div>
                     <div id="time" className='bg-[#E75C43] w-[28vw] rounded-4xl flex items-center justify-center'>
-                        <h1 className='text-7xl text-[#404040] font-flubber pt-7'>00:00:00</h1>
+                        <h1 className='text-7xl text-textgrey font-flubber pt-7'>00:00:00</h1>
                     </div>
                 </div>
             </div>
--- frontend/src/components/Features.jsx
+++ frontend/src/components/Features.jsx
@@ -1,7 +1,7 @@
 const Features = () => {
     return (
         <div className="h-screen w-screen flex items-center justify-center">
-            <h1 className="font-flubber text-coalblack text-6xl">Features.</h1>
+            <h1 className="font-flubber text-coalblack text-6xl dropDown">Features.</h1>
         </div>
     );
 }
