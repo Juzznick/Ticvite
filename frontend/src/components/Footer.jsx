9ff87b5a8a892a5f97d06a5868f8a9b7790a6574
Juzznick <xnick126@gmail.com>
2025-10-15 00:45:05 +0400

    font change

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <div className="w-screen flex flex-col items-center bg-litegreen h-[40vh] justify-center font-bold">
-            <h1 className="text-textgrey font-inter text-2xl">TicVite</h1>
+            <h1 className="text-textgrey font-flubber text-4xl tracking-widest">TicVite</h1>
             <ul className="flex w-[30vw] justify-around mt-15 text-textgrey">
                 <li>HOME</li>
                 <li>FEATURES</li>
