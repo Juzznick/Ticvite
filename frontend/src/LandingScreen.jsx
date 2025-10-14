a40a4aea2a5d0b57187021459473adc723650a0d
Juzznick <xnick126@gmail.com>
2025-10-15 00:35:31 +0400

    added footer

--- frontend/src/LandingScreen.jsx
+++ frontend/src/LandingScreen.jsx
@@ -1,13 +1,14 @@
 import Navbar from "./components/Navbar";
 import Features from "./components/features";
 import Home from "./components/Home";
+import Footer from "./components/Footer";
 
 const LandingScreen = () => {
     return (
         <div >
-            <Navbar/>
-            <Home/>
-            <Features />
+            <Footer/>
+            {/* <Home/>
+            <Features /> */}
         </div>
 
     );
